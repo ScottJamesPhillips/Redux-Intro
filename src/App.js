d9da602dcd9429b90d8cf189ce2031c14e6c0a30
Scott Phillips <Scott.Phillips@core-uk.com>
2022-03-09 14:56:39 +0000

    Begin introduction of Redux

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import M from "materialize-css/dist/js/materialize.min.js";
 import "./App.css";
 import SearchBar from "./components/layout/SearchBar";
 import Logs from "./components/logs/Logs";
-import AddBtn from "./components/layout/AddBtn"
-import AddLogModal from "./components/logs/AddLogModal"
-import EditLogModal from "./components/logs/EditLogModal"
-import AddTechModal from "./components/techs/AddTechModal"
-import TechListModal from "./components/techs/TechListModal"
+import AddBtn from "./components/layout/AddBtn";
+import AddLogModal from "./components/logs/AddLogModal";
+import EditLogModal from "./components/logs/EditLogModal";
+import AddTechModal from "./components/techs/AddTechModal";
+import TechListModal from "./components/techs/TechListModal";
+import { Provider } from "react-redux";
+import store from "./components/store";
 
 const App = () => {
   useEffect(() => {
@@ -16,17 +18,19 @@ const App = () => {
     M.AutoInit();
   });
   return (
-    <Fragment>
-      <SearchBar />
-      <div className="container">
-        <AddBtn/>
-        <AddLogModal/>
-        <EditLogModal/>
-        <AddTechModal/>
-        <TechListModal/>
-        <Logs />
-      </div>
-    </Fragment>
+    <Provider store={store}>
+      <Fragment>
+        <SearchBar />
+        <div className="container">
+          <AddBtn />
+          <AddLogModal />
+          <EditLogModal />
+          <AddTechModal />
+          <TechListModal />
+          <Logs />
+        </div>
+      </Fragment>
+    </Provider>
   );
 };
 
