34117abc0061e6f0e0c85673f2a3c8232ba748ad
Scott Phillips <36128399+ScottJamesPhillips@users.noreply.github.com>
2022-03-10 16:56:57 +0000

    Implement search functionality

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -6,7 +6,8 @@ import {
   DELETE_LOG,
   UPDATE_LOG,
   SET_CURRENT,
-  CLEAR_CURRENT
+  CLEAR_CURRENT,
+  SEARCH_LOGS
 } from "./types";
 
 
@@ -27,6 +28,26 @@ export const getLogs = () => async (dispatch) => {
     })
   }
 };
+
+
+//Search server logs
+  export const searchLogs = text => async dispatch => {
+    try {
+      setLoading();
+      const res = await fetch(`/logs?q=${text}`);
+      const data = await res.json();
+      dispatch({
+        type: SEARCH_LOGS,
+        payload: data,
+      });
+
+    } catch (err) {
+      dispatch({
+        type: LOGS_ERROR,
+        payload: err.response.data
+      })
+    }
+  };
 
 
 //Add new log
