8c5b4cbe5a8f5b589b9794a3d1e2829fad2253f6
scottphillipsmoov <scott.phillips@moov.tv>
2022-03-16 15:16:11 +0000

    Add tech route completed

--- src/components/techs/AddTechModal.js
+++ src/components/techs/AddTechModal.js
@@ -1,15 +1,19 @@
 import React, {useState} from "react";
 import M from "materialize-css/dist/js/materialize.min.js"
+import {connect} from "react-redux";
+import PropTypes from "prop-types";
+import {addTech} from "../../actions/techActions"
 
-const AddTechModal = () => {
+const AddTechModal = ({addTech}) => {
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
 
   const onSubmit = () => {
     if (firstName === "" || lastName === "") {
-      M.toast({ html: "Please enter a firt and last name." });
+      M.toast({ html: "Please enter a first and last name." });
     } else {
-      console.log(firstName, lastName);
+      addTech({firstName, lastName})
+      M.toast({ html: `${firstName} ${lastName} was added as a tech.` });
       setFirstName("");
       setLastName("");
     }
@@ -60,4 +64,8 @@ const AddTechModal = () => {
   );
 };
 
-export default AddTechModal;
+AddTechModal.propTypes = {
+  addTech:PropTypes.func.isRequired,
+}
+
+export default connect(null, {addTech})(AddTechModal);
